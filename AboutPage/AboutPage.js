e4690e897a3d92834673734d97e0d9dd4c41763c
Tshepiso Motshoene <tshepisosabelo29@gmail.com>
2024-12-14 03:45:14 +0200

    CREATE AboutPage.js
    Made aboutpage.js a module

---
+++ AboutPage/AboutPage.js
@@ -0,0 +1,9 @@
+let CustomerName = document.getElementById('CustomerName');
+let CustomerSurname = document.getElementById('CustomerSurname');
+let CustomerPhone = document.getElementById('CustomerPhone');
+let CustomerEmail = document.getElementById('CustomerEmail');
+let BookedService = document.getElementById('bookedService');
+let bookedDay = document.getElementById('bookedDay');
+
+let CustomerInfo = [CustomerName, CustomerSurname, CustomerPhone, CustomerEmail, BookedService, bookedDay];
+export default CustomerInfo;
